cab30971310c69976ffaa8f46b6b28cd63e70318
jotabraga <jpmbraga.feg@gmail.com>
2021-08-25 13:04:11 -0300

    Remove border to style improvement

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -62,7 +62,6 @@ const StyledHeader = styled.div`
   align-items: center;
   background-color: #ef5798;
   color: #fff;
-  border-bottom: 0.4px solid #000;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.45), 0 6px 20px 0 rgba(0, 0, 0, 0.35);
   z-index: 15;
   @media (max-width: 611px) {
